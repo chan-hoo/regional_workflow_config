c335b9a6e4697bad70bd9bfce0bd9d08d90d2006
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2021-04-01 08:19:45 -0400

    Update and rename config_nco_conus_25km_gfdl.sh to config_nco_conus25gfdl.sh

--- wcoss_cray/config_nco_conus25gfdl.sh
+++ wcoss_cray/config_nco_conus25gfdl.sh
@@ -7,6 +7,9 @@ VERBOSE="TRUE"
 RUN_ENVIR="nco"
 PREEXISTING_DIR_METHOD="rename"
 
+USE_CRON_TO_RELAUNCH="TRUE"
+CRON_RELAUNCH_INTVL_MNTS="5"
+
 PREDEF_GRID_NAME="CONUS_25km_GFDLgrid"
 QUILTING="TRUE"
 
@@ -15,8 +18,8 @@ CCPP_PHYS_SUITE="FV3_GFS_v15p2"
 FCST_LEN_HRS="06"
 LBC_SPEC_INTVL_HRS="3"
 
-DATE_FIRST_CYCL="20210110"
-DATE_LAST_CYCL="20210110"
+DATE_FIRST_CYCL="20210330"
+DATE_LAST_CYCL="20210330"
 CYCL_HRS=( "00" )
 
 EXTRN_MDL_NAME_ICS="FV3GFS"
@@ -24,6 +27,6 @@ EXTRN_MDL_NAME_LBCS="FV3GFS"
 
 RUN="srw_nco"
 COMINgfs="/gpfs/dell1/nco/ops/com/gfs/prod"
-FIXLAM_NCO_BASEDIR="/gpfs/hps3/emc/meso/noscrub/Chan-Hoo.Jeon/00_DATA/FV3LAM_pregen"
+FIXLAM_NCO_BASEDIR="/gpfs/hps3/emc/meso/noscrub/UFS_SRW_App/FV3LAM_pregen"
 STMP="/gpfs/hps3/emc/meso/noscrub/Chan-Hoo.Jeon/01_OUT_DATA/stmp"
 PTMP="/gpfs/hps3/emc/meso/noscrub/Chan-Hoo.Jeon/01_OUT_DATA/ptmp"
