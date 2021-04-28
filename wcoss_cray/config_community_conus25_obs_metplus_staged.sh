91f86306297f7b132baad5fa715f20feda1eec5a
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2021-04-28 08:56:22 -0400

    Update config_community_conus25_obs_metplus_staged.sh

--- wcoss_cray/config_community_conus25_obs_metplus_staged.sh
+++ wcoss_cray/config_community_conus25_obs_metplus_staged.sh
@@ -17,8 +17,8 @@ CCPP_PHYS_SUITE="FV3_GFS_v15p2"
 FCST_LEN_HRS="24"
 LBC_SPEC_INTVL_HRS="6"
 
-DATE_FIRST_CYCL="20210423"
-DATE_LAST_CYCL="20210423"
+DATE_FIRST_CYCL="20190615"
+DATE_LAST_CYCL="20190615"
 CYCL_HRS=( "00" )
 
 EXTRN_MDL_NAME_ICS="FV3GFS"
@@ -32,13 +32,19 @@ WTIME_RUN_FCST="02:30:00"
 MODEL="FV3_GFS_v15p2_CONUS_25km"
 METPLUS_PATH="/gpfs/hps/nco/ops/nwprod/metplus.v3.1.1/METplus-3.1.1"
 MET_INSTALL_DIR="/gpfs/hps/nco/ops/nwprod/met.v9.1.3"
-CCPA_OBS_DIR="/gpfs/hps3/emc/meso/noscrub/Chan-Hoo.Jeon/01_OUTPUT/obs_data/ccpa/data"
-MRMS_OBS_DIR="/gpfs/hps3/emc/meso/noscrub/Chan-Hoo.Jeon/01_OUTPUT/obs_data/mrms/data"
-NDAS_OBS_DIR="/gpfs/hps3/emc/meso/noscrub/Chan-Hoo.Jeon/01_OUTPUT/obs_data/ndas/data"
+CCPA_OBS_DIR="/gpfs/hps3/emc/meso/noscrub/UFS_SRW_App/obs_data/ccpa/proc"
+MRMS_OBS_DIR="/gpfs/hps3/emc/meso/noscrub/UFS_SRW_App/obs_data/mrms/proc"
+NDAS_OBS_DIR="/gpfs/hps3/emc/meso/noscrub/UFS_SRW_App/obs_data/ndas/proc"
 
 RUN_TASK_GET_OBS_CCPA="TRUE"
 RUN_TASK_GET_OBS_MRMS="TRUE"
 RUN_TASK_GET_OBS_NDAS="TRUE"
 RUN_TASK_VX_GRIDSTAT="TRUE"
 RUN_TASK_VX_POINTSTAT="TRUE"
+
+USE_USER_STAGED_EXTRN_FILES="TRUE"
+EXTRN_MDL_SOURCE_BASEDIR_ICS="/gpfs/hps3/emc/meso/noscrub/UFS_SRW_App/model_data/FV3GFS"
+EXTRN_MDL_FILES_ICS=( "gfs.pgrb2.0p25.f000" )
+EXTRN_MDL_SOURCE_BASEDIR_LBCS="/gpfs/hps3/emc/meso/noscrub/UFS_SRW_App/model_data/FV3GFS"
+EXTRN_MDL_FILES_LBCS=( "gfs.pgrb2.0p25.f006" "gfs.pgrb2.0p25.f012" "gfs.pgrb2.0p25.f018" "gfs.pgrb2.0p25.f024" )
 
